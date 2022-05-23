1b0160c54f42ff08d504efd06eb59835452b5592
Darya Ignateva <dashama19ig02@mail.ru>
2022-05-23 21:14:46 +0400

    game
    game

---
+++ game/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" onclick="jump()">
+<head>
+    <meta charset="UTF-8">
+    <title>Ssendem</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div id="game">
+    <div id="block"></div>
+    <div id="hole"></div>
+    <div id="character"></div>
+</div>
+
+</body>
+<script src="js/game.js"></script>
+</html>
